2914487a6a231980142d0f4ea72e08f7b956429e
Michael Stocker <mic.sto@hotmail.com>
2016-10-31 20:55:58 +0100

    Initial commit with (not)working example

---
+++ src/issue/spoon_precompile/SimpleProcessor.java
@@ -0,0 +1,18 @@
+package issue.spoon_precompile;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.declaration.CtType;
+
+/**
+ * Simple Processor to demonstrate <code>--precompile</code> issue
+ * 
+ * @author Michael Stocker
+ * @since 0.1.0
+ */
+public class SimpleProcessor extends AbstractProcessor<CtType<?>> {
+
+    @Override
+    public void process(CtType<?> element) {
+        System.out.println(">> Hello: " + element.getSimpleName() + " <<");
+    }
+}
